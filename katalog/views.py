60778e134c1167e0dcdcd82ebb4741ab72041b45
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-09-13 14:31:44 +0700

    Edit views.py

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,16 @@
+from gettext import Catalog
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+# Sebuah fungsi yang menerima parameter request dan mengembalikan render(request, "katalog.html")
+def show_catalog(request):
+    catalog_data = CatalogItem.objects.all()
+    # name_header = Name.objects.all()
+    
+    context = {
+        'list_data': catalog_data,
+        'nama':  'Ramya Nareswari',
+        'id': '2106751606'
+    }
+
+    return render(request, "katalog.html", context)
