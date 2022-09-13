a5938dbff67f95a86f7ec9bd1fb33e1526ee51c0
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-09-13 14:52:16 +0700

    Edit context dict in views.py katalog app

--- katalog/views.py
+++ katalog/views.py
@@ -2,14 +2,12 @@ from gettext import Catalog
 from django.shortcuts import render
 from katalog.models import CatalogItem
 
-# Sebuah fungsi yang menerima parameter request dan mengembalikan render(request, "katalog.html")
 def show_catalog(request):
     catalog_data = CatalogItem.objects.all()
-    # name_header = Name.objects.all()
     
     context = {
         'list_data': catalog_data,
-        'nama':  'Ramya Nareswari',
+        'name':  'RAMYA NARESWARI',
         'id': '2106751606'
     }
 
