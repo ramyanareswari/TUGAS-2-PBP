6d8759e8b0b92e0a6551c59a6447a2098c8d40a6
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-09-21 18:34:23 +0700

    Make mywatchlist app

---
+++ mywatchlist/models.py
@@ -0,0 +1,11 @@
+from pyexpat import model
+from turtle import title
+from django.db import models
+
+# Create your models here.
+class WatchListItem(models.Model):
+    watched = models.BooleanField()
+    title = models.TextField()
+    rating = models.IntegerField()
+    release_date = models.DateField()
+    review = models.TextField()
