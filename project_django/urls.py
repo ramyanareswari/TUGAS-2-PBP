72a563ca550dea7cb64f253c688838d432e5d4e1
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-09-22 02:12:39 +0700

    Edited urls.py

--- project_django/urls.py
+++ project_django/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
     path('katalog/', include('katalog.urls')),
-    path('mywatchlist', include('mywatchlist.urls')),
+    path('mywatchlist/', include('mywatchlist.urls')),
 ]
 
