203a7d86ef0d11ccf7acd813c5ba585ffaefddc9
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-09-21 18:52:05 +0700

    Deleted db.sqlite3

---
+++ mywatchlist/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+# Create your tests here.
+# class Tugas2UnitTesting(TestCase):
+#     def test_page(self):
+#         response = Client().get('/mywatchlist/')
+#         self.assertEqual(response.status_code,200)
