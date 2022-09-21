e8727a0b9704c0e340d0d1bfbebf407d9116e745
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-09-21 18:46:07 +0700

    Configure urls.py in mywatchlist app

---
+++ mywatchlist/urls.py
@@ -0,0 +1,9 @@
+# Implementasi routing terhadap views.py
+from django.urls import path
+from mywatchlist.views import show_watchlist
+
+app_name = 'mywatchlist'
+
+urlpatterns = [
+    path('', show_watchlist, name='show_watchlist'),
+]
