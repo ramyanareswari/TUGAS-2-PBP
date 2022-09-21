e14006034d43b877d2fdc4414662816726dd2f07
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-09-22 02:14:58 +0700

    Add unit testing

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,7 +1,19 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
-# class Tugas2UnitTesting(TestCase):
-#     def test_page(self):
-#         response = Client().get('/mywatchlist/')
-#         self.assertEqual(response.status_code,200)
+class TestMyWatchlist(TestCase):
+    def test_mywatchlist(self):
+        response = Client.get('/mywatchlist/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_xml(self):
+        response = Client.get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_json(self):
+        response = Client.get('/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_html(self):
+        response = Client.get('/mywatchlist/html/')
+        self.assertEqual(response.status_code, 200)
