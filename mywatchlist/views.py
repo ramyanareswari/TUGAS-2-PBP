9bfb67ed0cb29506a523cb08c33b4b438b0ec63c
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-09-22 02:27:33 +0700

    Edited views.py

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+from django.core import serializers
+from django.http import HttpResponse, request
 from django.shortcuts import render
 from mywatchlist.models import WatchListItem
 
@@ -8,7 +10,45 @@ def show_watchlist(request):
     context = {
         'watchlist_item': watchlist_item,
         'name':  'RAMYA NARESWARI',
-        'id': '2106751606'
+        'id': '2106751606',
     }
 
     return render(request, "watchlist.html", context)
+
+def show_html(request):
+    data = WatchListItem.objects.all()
+    # Display message
+    watched_count = data.filter(watched=True).count()
+    unwatched_count = data.filter(watched=False).count()
+
+    if watched_count > unwatched_count:
+        message = "Selamat, kamu sudah banyak menonton!"
+
+    else:
+        message = "Wah, kamu masih sedikit menonton!"
+
+    context = {
+        'watchlist_item': data,
+        'name':  'RAMYA NARESWARI',
+        'id': '2106751606',
+        'message': message
+    }
+    return render(request, "watchlist.html", context)
+
+def show_xml(request):
+    data = WatchListItem.objects.all()
+    
+    return HttpResponse(serializers.serialize('xml', data), content_type='application/xml')
+
+def show_json(request):
+    data = WatchListItem.objects.all()
+    
+    return HttpResponse(serializers.serialize('json', data), content_type='application/json')
+
+def show_json_id(request, id):
+    data = WatchListItem.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_id(request, id):
+    data = WatchListItem.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
