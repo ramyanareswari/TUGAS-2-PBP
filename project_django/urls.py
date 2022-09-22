4b1e8d4a37d4994874b94de5a9cfea856e340a3b
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-09-22 08:28:35 +0700

    change

--- project_django/urls.py
+++ project_django/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('', include('example_app.urls')),
     path('katalog/', include('katalog.urls')),
     path('mywatchlist/', include('mywatchlist.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
