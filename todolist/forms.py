97ea725d36dff7c180a775bdab2c9731e4ddcc70
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-10-05 12:26:11 +0700

    Tugas 5 First Version

--- todolist/forms.py
+++ todolist/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 from todolist.models import Task
+from django.forms import TextInput
+
 
 class CreateNew(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description']
+
+        widgets = {
+            'title': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Insert task title..'
+                }),
+            'description': TextInput(attrs={
+                'class': "form-control", 
+                'style': 'max-width: 300px;',
+                'placeholder': 'Insert task description..'
+                })
+        }
